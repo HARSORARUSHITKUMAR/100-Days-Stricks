cc5bef4b93cf95a439bdff9fd5c756aaa5737960
Rushit Harsora <rushitharsora@gmail.com>
2023-05-02 00:18:56 +0530

    first commit

---
+++ assiop.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=5;
+    int b=6;
+    //a=b;
+    //long hand op
+    /*a=a+b;*/
+    // short hand op
+    //a += b;
+   // a -= b;
+   // a *= b;
+   //float a=5;
+   //float b=6;
+    //a /= b;
+    a %= b;
+    
+    printf("%d",a);
+    return 0;
+
+}
