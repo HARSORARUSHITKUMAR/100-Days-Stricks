023a898294c738a8cd0b0301933c6dfdce2bceff
Rushit Harsora <rushitharsora@gmail.com>
2023-06-03 23:48:16 +0530

    main

---
+++ sum.js
@@ -0,0 +1,12 @@
+function sumOfIntegers(a, b) {
+    if (a === b) {
+      return 3 * (a + b);
+    } else {
+      return a + b;
+    }
+  }
+  
+  // Example usage:
+  console.log(sumOfIntegers(3, 5)); // Output: 8
+  console.log(sumOfIntegers(2, 2)); // Output: 12
+  
