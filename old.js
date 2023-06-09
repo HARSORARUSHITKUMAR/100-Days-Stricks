5778ed3ca7736c875fccd3bfb4fffcbb3903517a
Rushit Harsora <rushitharsora@gmail.com>
2023-06-09 18:45:51 +0530

    main

---
+++ old.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const encodedParams = new URLSearchParams();
+encodedParams.set('userId', '<REQUIRED>');
+encodedParams.set('accessToken', '<REQUIRED>');
+encodedParams.set('playlistId', '<REQUIRED>');
+
+const options = {
+  method: 'POST',
+  url: 'https://spotifystefan-skliarovv1.p.rapidapi.com/addTracksToPlaylist',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'Spotifystefan-skliarovV1.p.rapidapi.com'
+  },
+  data: encodedParams,
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
+
